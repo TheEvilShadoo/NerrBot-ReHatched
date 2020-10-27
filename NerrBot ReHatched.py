d77d05946eff08601415a90b624c71ae0fa32082
TheEvilShadoo <ryan-skis-18@outlook.com>
2020-10-27 01:47:03 -0400

    Update NerrBot ReHatched.py

--- NerrBot ReHatched.py
+++ NerrBot ReHatched.py
@@ -55,7 +55,7 @@ class Namespace(BaseNamespace):
         sio.wait(seconds=.2)
         sio.emit("posts:index", {"topic": False, "room": "sidebar", "topicsOnly": False, "source": "db"}, Namespace.on_the_dump_index_response)
         sio.wait(seconds=.2)
-        sio.emit("posts:chats", {"room": "sidebar"}, Namespace.on_NerrChat_response)
+        sio.emit("posts:chats", {"room": "sidebar"}, Namespace.on_NerrChat_chatlog_response)
 
     def on_authentication(self):
         logging.info("Successfully logged in")
@@ -63,22 +63,62 @@ class Namespace(BaseNamespace):
 
     def on_disconnect(self):
         logging.warning('The Digibutter websocket has closed the connection.')
+        print('\n> The Digibutter websocket has closed the connection.')
 
     def on_all_posts_index_response(self):
         print('\n> Received posts index for room "All Posts"')
-        index = json5.loads(str(self))
+        print('\n> Parsing index...')
+        try:
+            index = json5.loads(str(self))
+            latest_post = index['posts'][0]['content']
+            print('\n> Index successfully parsed')
+        except:
+            logging.error('Failed to parse index')
+            print('\n> ERROR: Failed to parse index')
 
     def on_gaming_news_index_response(self):
         print('\n> Received posts index for room "Gaming News"')
-        index = json5.loads(str(self))
+        print('\n> Parsing index...')
+        try:
+            index = json5.loads(str(self))
+            latest_post = index['posts'][0]['content']
+            print('\n> Index successfully parsed')
+        except:
+            logging.error('Failed to parse index')
+            print('\n> ERROR: Failed to parse index')
 
     def on_the_dump_index_response(self):
         print('\n> Received posts index for room "The Dump"')
-        index = json5.loads(str(self))
+        print('\n> Parsing index...')
+        try:
+            index = json5.loads(str(self))
+            latest_post = index['posts'][0]['content']
+            print('\n> Index successfully parsed')
+        except:
+            logging.error('Failed to parse index')
+            print('\n> ERROR: Failed to parse index')
 
-    def on_NerrChat_response(self):
+    def on_NerrChat_chatlog_response(self):
         print('\n> Received chatlog for "NerrChat"')
-        index = json5.loads(str(self))
+        print('\n> Parsing index...')
+        try:
+            index = json5.loads(str(self))
+            latest_post = index['posts'][0]['content']
+            print('\n> Index successfully parsed')
+        except:
+            logging.error('Failed to parse index')
+            print('\n> ERROR: Failed to parse index')
+
+    def on_new_post(self):
+        print('\n> Received a new post')
+        print('\n> Parsing content...')
+        try:
+            latest_post = json5.loads(str(self))
+            content = latest_post['content']
+            print(latest_post)
+        except:
+            logging.error('Failed to parse post content')
+            print('\n> ERROR: Failed to parse post content')
 
     def authenticate(self):
         logging.info("Attempting to login...")
@@ -86,7 +126,7 @@ class Namespace(BaseNamespace):
         sio.emit("ready", Namespace.on_authentication(BaseNamespace))
 
     def disconnect_if_inactive(self):
-        sio.wait(seconds=100)
+        sio.wait(seconds=10000)
         sio.disconnect()
         
 class room_name_ids:
@@ -96,4 +136,7 @@ class room_name_ids:
     Nerr_Chat = "posts:chats" 
 
 sio = SocketIO('http://digibutter.nerr.biz', 80, Namespace, cookies={'nerr3': "s:uW83D8OONzshQkshsXgwYiZG.GhLY3EKzpIt6tuZtsLcfiWpfu4ze5QsHkZ8gfQtKDHM"})
+sio.on('posts:create', Namespace.on_new_post)
+sio.wait(seconds=5)
+#sio.emit('posts:create', {"content":"I control NerrChat now! Mwahahahahahahaha!", "post_type":"chat","roomId":"sidebar","source":"db"})
 Namespace.disconnect_if_inactive(Namespace)
