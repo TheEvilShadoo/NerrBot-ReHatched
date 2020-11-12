013da21e86ffb6d002c355865be205e5e908bb33
TheEvilShadoo <ryan-skis-18@outlook.com>
2020-11-12 00:03:20 -0500

    Added support for custom rolls

--- utilities/dice.py
+++ utilities/dice.py
@@ -1,8 +1,15 @@
+from os import makedirs
 import random
 
 def default_roll():
     min_value = 1
     max_value = 6
 
+    roll = random.randint(min_value, max_value)
+    return roll
+
+def custom_roll(max_value):
+    min_value = 1
+    
     roll = random.randint(min_value, max_value)
     return roll
